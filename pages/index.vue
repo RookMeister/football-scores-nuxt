e7d81082361efbf3ff7112b93576284cf1108a31
RookMeister <rookmeister666@gmail.com>
2024-04-11 20:52:54 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
 import ListLeagues from '@/components/List.vue'
 import Matches from '@/components/Matches.vue'
-// import { useMatchesStore } from '@/modules/matches/store'
 
-// definePageMeta({
-//   layout: 'matches',
-// })
-// const store = useMatchesStore()
-
-const { pending, error, data: leagues } = await useFetch(() => `/api/allLeagues/`, { method: 'GET' })
+const { data: leagues } = await useFetch(() => `/api/allLeagues/`, { method: 'GET' })
 const { data: matches } = await useFetch(() => `/api/matches/20240410`, { method: 'GET' })
-
-// const mapLeagues = computed(() => {
-// 	if (!leagues.value) return {}
-// 	const leaguesCountries = leagues.value.countries.map(l => l.leagues.flat())
-// 	const leaguesInternal = leagues.value.international.map(l => l.leagues.flat())
-// 	return Object.fromEntries([...leaguesCountries, ...leaguesInternal].flat().map(l => ([l.id, l])))
-// 	// return [...leaguesCountries, ...leaguesInternal].flat()
-// 	// leagues.value ?Object.fromEntries(leagues.value.countries.map(l => ([l.id, l]))) : {}
-// })
 </script>
 
 <template>
